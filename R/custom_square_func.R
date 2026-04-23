825240deeb7ac78afb6cd68bd6ab6b44de0364f4
linnkyos <kzlin@uw.edu>
2026-04-23 10:31:08 -0700

    My errorenous change

---
+++ R/custom_square_func.R
@@ -0,0 +1,28 @@
+#' Squared function
+#'
+#' This is my demo squared function. 
+#' This is my demo squared function. 
+#' This is my demo squared function. 
+#' This is my demo squared function. 
+#' This is my demo squared function. 
+#' This is my demo squared function. 
+#' This is my demo squared function. 
+#' This is my demo squared function. 
+#' v
+#' This is my demo squared function. 
+#' This is my demo squared function. 
+#' This is my demo squared function. 
+#' v
+#' v
+#' This is my demo squared function. 
+#' 
+#' @param x scalar value
+#'
+#' @returns a scalar value
+#' @export
+#'
+#' @examples
+#' custom_square_func(10)
+custom_square_func <- function(x){
+  x^2
+}
