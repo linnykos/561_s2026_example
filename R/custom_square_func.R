630a5347782e6a85fe35cbc2a18478fbd4435ae6
linnkyos <kzlin@uw.edu>
2026-04-23 11:33:00 -0700

    Fixing pkg

--- R/custom_square_func.R
+++ R/custom_square_func.R
@@ -1,28 +1,3 @@
-#' Squared function
-#'
-#' This is my demo squared function. 
-#' This is my demo squared function. 
-#' This is my demo squared function. 
-#' This is my demo squared function. 
-#' This is my demo squared function. 
-#' This is my demo squared function. 
-#' This is my demo squared function. 
-#' This is my demo squared function. 
-#' v
-#' This is my demo squared function. 
-#' This is my demo squared function. 
-#' This is my demo squared function. 
-#' v
-#' v
-#' This is my demo squared function. 
-#' 
-#' @param x scalar value
-#'
-#' @returns a scalar value
-#' @export
-#'
-#' @examples
-#' custom_square_func(10)
 custom_square_func <- function(x){
-  x^2
+  if(is.numeric(x)) return(x^2)
 }
