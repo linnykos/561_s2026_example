02f9513398cd0940aecc2b82d2b73ee615795391
linnkyos <kzlin@uw.edu>
2026-05-28 06:37:34 +0300

    Updating

--- R/custom_square_func.R
+++
@@ -1,3 +0,0 @@
-custom_square_func <- function(x){
-  if(is.numeric(x)) return(x^2)
-}
--- R/demo-tibble.R
+++
@@ -1,9 +0,0 @@
-.demo_tibble <- function(){
-  data <- data.frame(a = 1:3,
-                     b = letters[1:3],
-                     c = Sys.Date())
-  data <- tibble::as_tibble(data)
-  tmp <- data %>% dplyr::filter(a == 1)
-
-  return(tmp)
-}
